eaedeac197bc0c1e7bbf5578786b041f5767b583
Raheem-Sk <raheemshaik276@gmail.com>
2023-06-20 22:40:45 +0530

    demo method

--- JAVA-FSD/src/practiceAssisted/Methods.java
+++ JAVA-FSD/src/practiceAssisted/Methods.java
@@ -3,8 +3,16 @@ package practiceAssisted;
 public class Methods {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//assigning result to Ans
+		int Ans = multiply(5,6);
+		System.out.println("Multiplictaion of two numbers is : "+Ans);
+			
+	}
+	// this the method that is called from main method
+	public static int multiply(int x,int y) {
+		int z = x*y;
+		return z;
+		
 	}
 
 }
