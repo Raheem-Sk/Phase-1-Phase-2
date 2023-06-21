76c046aa4918f898598e6fcbee64038c547f39c9
Raheem-Sk <raheemshaik276@gmail.com>
2023-06-21 13:43:50 +0530

    constructor with parameters

--- JAVA-FSD/src/practiceAssisted/ConstructorTypes.java
+++ JAVA-FSD/src/practiceAssisted/ConstructorTypes.java
@@ -4,9 +4,19 @@ public class ConstructorTypes {
 
 	public static void main(String[] args) {
 		// creating a object
+		// 1st instance object of class student 
 		Student s1 = new Student();
-		//calling a method
+		// 2nd instance object of class student 
+		Student s2 = new Student();
+		//calling a 1st object & its methods with constructors
+		System.out.print(s1+"\n");
+		s1.getInfo(1, "Raheem");
 		s1.details();
+		//calling a 1st object & its methods with constructors
+		System.out.print(s2+"\n");
+		s2.getInfo(2, "Shaik");
+		s2.details();
+
 
 	}
 
@@ -18,5 +28,18 @@ class Student {
 	// this constructor does not have any parameters so default values will be assigned by Javac.
 	void details() {
 		System.out.println("Student "+name+" is having a regno : "+regNo);
+		
 	}
+	void getInfo(int r, String n) {
+		this.regNo = r;
+		this.name = n;
+	}
+	
 }
+
+
+
+
+
+
+
