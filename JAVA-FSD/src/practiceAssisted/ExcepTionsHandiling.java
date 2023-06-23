92c6a3d9bfb60353fdf4c789097e3ebe4400a539
Raheem-Sk <raheemshaik276@gmail.com>
2023-06-23 10:06:02 +0530

    try,catch,throw,throws,finally,custom exceptions

--- JAVA-FSD/src/practiceAssisted/ExcepTionsHandiling.java
+++ JAVA-FSD/src/practiceAssisted/ExcepTionsHandiling.java
@@ -9,24 +9,29 @@ class InvalidMailException extends Exception{
 }
 }
 public class ExcepTionsHandiling{  		
-		
+		//using throws keyword to a method to catch custom throw
 		static void Validte(String S) throws InvalidMailException {
 	
 			if(S.contains("@gmail.com"))
 				System.out.println("Welcome people");
 			else
-				//throwing an error when mailId not having @gmail.com
+				//throwing a custom error when mailId not having @gmail.com
 				throw new InvalidMailException("Not a Valid google mail ID");
 }
 
 		public static void main(String[] args) {
 			// this block will try if code is tend to give exception
 			try {
-				Validte("shaik@gmail.com");
-				//catch block  will catch those errors
+				Validte("shaikgmail.com");
+			//catch block  will catch those errors
 			}
 			catch (InvalidMailException em) {
+				//printing the exception
 				System.out.println("Exception occured "+em);
+			}
+			//finally block get executed irrespective of Exception caught or not
+			finally {
+				System.out.println("Finally exception is caught");
 			}
 		}
 }
