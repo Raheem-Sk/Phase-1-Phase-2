20f356172cb85162135d4147c52d0ee8108e5d7f
Raheem-Sk <raheemshaik276@gmail.com>
2023-06-23 09:48:08 +0530

    Coustum exceptions

--- JAVA-FSD/src/practiceAssisted/ExcepTionsHandiling.java
+++ JAVA-FSD/src/practiceAssisted/ExcepTionsHandiling.java
@@ -1,19 +1,32 @@
 package practiceAssisted;
 
-public class ExcepTionsHandiling  {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int a= 20;
-		int b= 0;
-		try {
-		int sum = a/b;
-		}
-		catch(ArithmeticException e) {
-			System.out.println(e);
-		}
-
-	}
-
+class InvalidMailException extends Exception{
+	
+	//constructor for custom exception
+	public InvalidMailException(String str) {
+		
+	
+}
+}
+public class ExcepTionsHandiling{  		
+		
+		static void Validte(String S) throws InvalidMailException {
+	
+			if(S.contains("@gmail.com"))
+				System.out.println("Welcome people");
+			else
+				//throwing an error when mailId not having @gmail.com
+				throw new InvalidMailException("Not a Valid google mail ID");
 }
 
+		public static void main(String[] args) {
+			// this block will try if code is tend to give exception
+			try {
+				Validte("shaik@gmail.com");
+				//catch block  will catch those errors
+			}
+			catch (InvalidMailException em) {
+				System.out.println("Exception occured "+em);
+			}
+		}
+}
