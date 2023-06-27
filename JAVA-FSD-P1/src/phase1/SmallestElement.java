493f22cf210028bd66c67759a7808243bab27c4f
Raheem-Sk <raheemshaik276@gmail.com>
2023-06-27 16:02:15 +0530

    Smallest kth element

---
+++ JAVA-FSD-P1/src/phase1/SmallestElement.java
@@ -0,0 +1,18 @@
+package phase1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SmallestElement {
+
+	public static void main(String[] args) {
+		int A[] = {11,14,28,30,24,5,96,17,86,39,100};
+		Arrays.sort(A);
+		System.out.println("Enter the K");
+		Scanner sc = new Scanner(System.in);
+		int k = sc.nextInt();
+		System.out.println(A[k-1]);
+	}	
+	
+
+}
