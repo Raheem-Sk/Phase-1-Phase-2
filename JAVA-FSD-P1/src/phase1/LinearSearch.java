78a0410749b5414069d1a015ad960b43e23b5c9c
Raheem-Sk <raheemshaik276@gmail.com>
2023-06-28 17:35:07 +0530

    linear search

---
+++ JAVA-FSD-P1/src/phase1/LinearSearch.java
@@ -0,0 +1,32 @@
+package phase1;
+
+import java.util.Scanner;
+
+public class LinearSearch {
+
+	public static void main(String[] args) {
+		int A[] = {1,2,3,4,5,6,7,8,9};
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the value that you want to find ?\n");
+		int key = s.nextInt();
+		boolean flag =false;
+		int i=0;
+		// for loop to traverse the Array
+		for (i=0;i<A.length;i++) 
+		{ // if to compare
+			if(A[i]==key) 
+			{ // when the value at index equals to key
+				flag =true;
+				break;
+			}
+		}
+		// when flag is true displays the index of the element.
+		if(flag==true)
+		System.out.println("Value found at index : "+i);
+		else
+		System.out.println("Value not found");
+		s.close();
+
+	}
+
+}
